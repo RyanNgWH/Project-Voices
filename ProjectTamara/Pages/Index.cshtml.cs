77a6b4ebcd46e6ce3ebdfa4fef990415527e7768
Jing Jie Chee <s10179578@connect.np.edu.sg>
2018-09-09 10:24:55 +0800

    .

--- ProjectTamara/Pages/Index.cshtml.cs
+++ ProjectTamara/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -14,14 +15,20 @@ namespace ProjectTamara.Pages
     {
         private readonly ProjectTamara.Models.ProjectTamaraContext _context;
         public List<Service> Service { get; set; }
+        public UserManager<BaseUser> _userManager { get; set; }
 
-        public IndexModel(ProjectTamara.Models.ProjectTamaraContext context)
+        public IndexModel(ProjectTamara.Models.ProjectTamaraContext context, UserManager<BaseUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public async Task<IActionResult> OnGetAsync()
         {
-            Service = await _context.Service.Where(p => (p.Status == "Confirmed")).ToListAsync();
+            if (User.Identity.IsAuthenticated)
+            {
+                var currentUser = await _userManager.GetUserAsync(User);
+                    Service = await _context.Service.Where(p => (p.Status == "Confirmed")).ToListAsync();
+            }
             return Page();
         }
     }
